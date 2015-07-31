9b973d194f4a47f966fa9c080d3054a30bca1b73
NancyKo <konancys@gmail.com>
2015-07-30 22:06:27 -0400

    Finished

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  (2..num).each do |divide|
+    if num == divide 
+    next 
+    elsif num % divide == 0
+      return false
+    else
+    end
+  end
+  true
+end
+
+
+
+
